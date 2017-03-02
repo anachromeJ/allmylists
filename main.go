c0d65e8fea3fec84cf9e84be900d902e46b962fb
Jincheng Li <jl4569@columbia.edu>
2017-03-01 19:34:38 -0500

    basic database connection and serving files

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
   "fmt"
   "net/http"
   "os"
+	"io/ioutil"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 func determineListenAddress() (string, error) {
@@ -14,8 +17,57 @@ func determineListenAddress() (string, error) {
   return ":" + port, nil
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World")
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := r.URL.String()
+
+	var bytes []byte
+	var err error
+	if filePath == "/" {
+		bytes, err = ioutil.ReadFile("frontend/src/index.html")
+	} else {
+		bytes, err = ioutil.ReadFile("frontend/src" + filePath)
+	}
+
+	// TODO: create a new stderr logger
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = w.Write(bytes)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func dbHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("select * from users")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	var (
+		email string
+		firstname string
+		lastname string
+		passhash string
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&email, &firstname, &lastname, &passhash)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(email, firstname, lastname, passhash)
+	}
+
+  fmt.Fprintln(w, "OK")
 }
 
 func main() {
@@ -23,7 +75,10 @@ func main() {
   if err != nil {
     log.Fatal(err)
   }
-  http.HandleFunc("/", hello)
+
+  http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/db", dbHandler)
+
   log.Printf("Listening on %s...\n", addr)
   if err := http.ListenAndServe(addr, nil); err != nil {
     panic(err)
